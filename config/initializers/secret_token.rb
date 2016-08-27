5a656bbdbf6f48eca069a69f9c1c09ab32c5a238
rochebox <rochebox@gmail.com>
2016-08-27 15:01:00 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homeowners::Application.config.secret_key_base = '37716bb9a93b5afbc550af539993426b1b0069457ba7a0d7a0c1107d7fa600ee0436e5142100b41901bdc470b5ab547e66b2efa69aeee96d3b11c32a29235160'
